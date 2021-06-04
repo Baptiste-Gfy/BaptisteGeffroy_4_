a32caaa2da435f52323bcb63fc77ff14175f8af7
baptiste_gfy <geffroy.baptiste@outlook.fr>
2021-06-04 16:54:46 +0200

    add comment

--- modal.js
+++ modal.js
@@ -1,3 +1,16 @@
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+const modalClose = document.querySelector(".close");
+
+// EVENT
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+// close modal event
+modalClose.addEventListener("click", closeModal);
+
+// FUNCTION
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -6,25 +19,11 @@ function editNav() {
     x.className = "topnav";
   }
 }
-
-// DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-const modalClose = document.querySelector(".close");
-
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// close modal event
-modalClose.addEventListener("click", closeModal);
-
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
-
-// close modal
+// close modal form
 function closeModal() {
   modalbg.style.display = "none";
 }
