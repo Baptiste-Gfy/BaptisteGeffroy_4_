36c2b9767be5c82674d9dbd3c99872427282074c
baptiste_gfy <geffroy.baptiste@outlook.fr>
2021-06-11 10:31:42 +0200

    add validation on firstName

--- modal.js
+++ modal.js
@@ -3,6 +3,8 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const modalClose = document.querySelector(".close");
+const firstNameInput = document.querySelector("#first");
+const nodeParentOfFirstName = firstNameInput.parentNode;
 
 // EVENT
 // launch modal event
@@ -13,11 +15,9 @@ modalClose.addEventListener("click", closeModal);
 // FUNCTION
 function editNav() {
   var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+  x.className === "topnav"
+    ? (x.className += " responsive")
+    : (x.className = "topnav");
 }
 // launch modal form
 function launchModal() {
@@ -28,19 +28,49 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
-const firstNameValid = () =>{
-  // Le champ Prénom a un minimum de 2 caractères / n'est pas vide.
-  // /^([a-zA-Z\sáàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ-]{2,})$/
-}
+const ValidadorFirstName = () => {
+  // min 2 character
+  // required
+  console.log(firstNameInput.value);
+  // =>
+  if (firstNameInput.value.length === 0) {
+    nodeParentOfFirstName.setAttribute("data-error", "Ce champs est requis");
+    nodeParentOfFirstName.setAttribute("data-error-visible", "true");
+  } else if (firstNameInput.value.length < 2) {
+    nodeParentOfFirstName.setAttribute(
+      "data-error",
+      "Ce choix doit contenir minimum 2 character"
+    );
+    nodeParentOfFirstName.setAttribute("data-error-visible", "true");
+  } else {
+    nodeParentOfFirstName.setAttribute("data-error-visible", "false");
+  }
+};
 
-const lastNameValid = () => {
-  // Le champ du nom de famille a un minimum de 2 caractères / n'est pas vide.
-  // /^([a-zA-Z\sáàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ-]{2,})$/
-}
+// const ValidadorLastName = () => {
+//   // min 2 character
+//   // required
+// };
 
-const emailValid = () => {
-  // L'adresse électronique est valide.
-  // ^([0-9a-zA-Z]([-_\\.]*[0-9a-zA-Z]+)*)@([0-9a-zA-Z]([-_\\.]*[0-9a-zA-Z]+)*)[\\.]([a-zA-Z]{2,9})$
-  // ^([\w]([-_\\.]*[\w]+)*)@([\w]([-_\\.]*[\w]+)*)[\\.]([a-zA-Z]{2,9})$
-  // ^[\w\-\.]+@([\w-]+\.)+[\w-]{2,3}$
-}
+// const ValidadorMail = () => {
+//   // L'adresse électronique est valide.
+//   // REGEX =>
+//   // ^([\w]([-_\\.]*[\w]+)*)@([\w]([-_\\.]*[\w]+)*)[\\.]([a-zA-Z]{2,9})$
+//   // required
+// };
+
+// const ValidadorBirthday = () => {
+//   // required
+// };
+
+// const ValidadorNbTournament = () => {
+//   // required
+// };
+
+// const ValidadorCity = () => {
+//   // required
+//   // maybe more city
+// };
+
+// TEST
+firstNameInput.addEventListener("blur", ValidadorFirstName);
